c093cbe7d65985ee5c6e6790883a13d338452bc9
jewelbaek <78218463+jewelbaek@users.noreply.github.com>
2021-03-02 04:06:49 -0800

    project

---
+++ studio_p2/main.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+    document.body.className += " loaded";
+}
+
+document.getElementById('shadows5').addEventListener('click', function () {
+  document.querySelector('#shadows1').classList.add('animated');
+});
+
+document.getElementById('shadows5').addEventListener('click', function () {
+  document.querySelector('#shadows2').classList.add('animated');
+});
+
+document.getElementById('shadows5').addEventListener('click', function () {
+  document.querySelector('#shadows3').classList.add('animated');
+});
+
+document.getElementById('shadows5').addEventListener('click', function () {
+  document.querySelector('#shadows4').classList.add('animated');
+});
+
+document.getElementById('shadows5').addEventListener('click', function () {
+  document.querySelector('#shadows5').classList.add('animated');
+});
+
+document.getElementById('shadows5').addEventListener('click', function () {
+  document.querySelector('#shadows6').classList.add('animated');
+});
+
+document.getElementById('shadows5').addEventListener('click', function () {
+  document.querySelector('#shadows7').classList.add('animated');
+});
+
+document.getElementById('shadows5').addEventListener('click', function () {
+  document.querySelector('#shadows8').classList.add('animated');
+});
+
+document.getElementById('shadows5').addEventListener('click', function () {
+  document.querySelector('#shadows9').classList.add('animated');
+});
+
+document.getElementById('shadows5').addEventListener('click', function () {
+  document.querySelector('.door').classList.add('animated');
+});
